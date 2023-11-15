f45a83fcd0e7df29ca2d19f554dea5b67dd7a55c
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-15 09:55:22 +0100

    Use functions in main

--- C/2023/KW46/LinkedList/src/main.c
+++ C/2023/KW46/LinkedList/src/main.c
@@ -6,5 +6,33 @@
 
 int main()
 {
+  TNode *head = random_linkedlist(10);
+
+  print_linkedlist(head);
+
+  printf("Remove node at index 5\n");
+  head = remove_node(head, 5, true);
+
+  print_linkedlist(head);
+
+  printf("Insert node at index 5\n");
+  TData data = {.value = 99};
+  head = insert_node(head, 5, data);
+
+  print_linkedlist(head);
+
+  printf("Append node\n");
+  data.value = 100;
+  head = append_node(head, data);
+
+  print_linkedlist(head);
+
+  TNode *node = get_node(head, 5);
+  printf("Node at index 5: %d\n", node->data.value);
+
+  printf("Node count: %zu\n", count_linkedlist(head));
+
+  free_linkedlist(head);
+
   return EXIT_SUCCESS;
 }
