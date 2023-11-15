73328cf1651403c2094bf5fabe5c0e65e047b360
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-15 09:53:47 +0100

    Set up LinkedList workspace

---
+++ C/2023/KW46/LinkedList/src/linkedlist.h
@@ -0,0 +1,9 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#endif // LINKEDLIST_H
---
+++ C/2023/KW46/LinkedList/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <linkedlist.h>
+
+int main()
+{
+  return EXIT_SUCCESS;
+}
