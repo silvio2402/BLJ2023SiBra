7c8877fd8f723715c06d324d78d48d0048edb444
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-22 13:32:12 +0100

    Add user input functionality

--- Java/2023/KW47/HelloWorld/src/App.java
+++ Java/2023/KW47/HelloWorld/src/App.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello, World!");
+
+        int a = in.nextInt();
+        System.out.println(a);
+
+        in.close();
     }
 }
