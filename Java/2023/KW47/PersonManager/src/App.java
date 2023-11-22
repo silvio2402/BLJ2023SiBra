a3997d7d27c1102bbd51e0ca7d352f4f475be1e9
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-22 14:18:40 +0100

    Set up java project

---
+++ Java/2023/KW47/PersonManager/src/App.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
+        in.close();
+    }
+}
