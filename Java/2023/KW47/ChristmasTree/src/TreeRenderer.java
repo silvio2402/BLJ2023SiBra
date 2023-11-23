9c34fd970814d3d105eab3d216d247b749f0286f
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-23 16:55:02 +0100

    Add colors and trunk

--- Java/2023/KW47/ChristmasTree/src/TreeRenderer.java
+++ Java/2023/KW47/ChristmasTree/src/TreeRenderer.java
@@ -1,7 +1,7 @@
 public class TreeRenderer {
     public Integer height = 0;
-    public Integer trunkWidth = 3;
-    public Integer trunkHeight = 2;
+    public Integer trunkWidth = 4;
+    public Integer trunkHeight = 3;
 
     public TreeRenderer(Integer height) {
         this.height = height;
@@ -16,17 +16,20 @@ public class TreeRenderer {
     public String render() {
         String result = "";
         // Tree
+        result += "\033[32m";
         for (int i = 0; i < this.height; i++) {
             result += " ".repeat(this.height - i - 1);
             result += "*".repeat(i * 2 + 1);
             result += "\n";
         }
         // Trunk
+        result += "\033[93m";
         for (int i = 0; i < this.trunkHeight; i++) {
             result += " ".repeat(this.height - this.trunkWidth / 2 - 1);
             result += "*".repeat(this.trunkWidth);
             result += "\n";
         }
+        result += "\033[0m";
         return result;
     }
 
