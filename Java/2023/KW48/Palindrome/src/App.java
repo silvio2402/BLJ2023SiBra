899190a42ac6803973d0685eed5e00fa4ed58a8c
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-29 10:30:32 +0100

    Setup project

---
+++ Java/2023/KW48/Palindrome/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("--- Palindrome Checker ---");
+
+        in.close();
+    }
+}
