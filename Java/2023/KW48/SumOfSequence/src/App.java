1f865e618fdc1914944274f42942a6064e8eb1f2
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-29 09:46:20 +0100

    Add calculation of the sum of a sequence

--- Java/2023/KW48/SumOfSequence/src/App.java
+++ Java/2023/KW48/SumOfSequence/src/App.java
@@ -4,8 +4,26 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Hello, World!");
+        System.out.print("From: ");
+        Long from = in.nextLong();
+        in.nextLine();
+
+        System.out.print("To: ");
+        Long to = in.nextLong();
+        in.nextLine();
+
+        Long sum = sumOfSequence(from, to);
+        System.out.println("Sum of sequence: " + sum);
 
         in.close();
+    }
+
+    public static Long sumOfSequence(Long from, Long to) {
+        Long sum = 0L;
+
+        for (Long i = from; i < to + 1; i++)
+            sum += i;
+
+        return sum;
     }
 }
