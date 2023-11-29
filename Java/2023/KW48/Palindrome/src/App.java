4cdbb3f2407b4abe581569595681050b6cbe1ddc
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-29 10:31:21 +0100

    Add palindrome checking functionality

--- Java/2023/KW48/Palindrome/src/App.java
+++ Java/2023/KW48/Palindrome/src/App.java
@@ -6,6 +6,33 @@ public class App {
 
         System.out.println("--- Palindrome Checker ---");
 
+        System.out.print("Enter a number: ");
+        long number = in.nextLong();
+        in.nextLine();
+
+        int length = getLength(number);
+        boolean isPalindrome = true;
+
+        for (int i = 1; i <= length / 2; i++) {
+            if (getDigit(number, i) != getDigit(number, length - i + 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome)
+            System.out.println(number + " is a palindrome.");
+        else
+            System.out.println(number + " is not a palindrome.");
+
         in.close();
+    }
+
+    public static int getDigit(long number, int position) {
+        return (int) (number / Math.pow(10, position - 1)) % 10;
+    }
+
+    public static int getLength(long number) {
+        return (int) Math.log10(number) + 1;
     }
 }
