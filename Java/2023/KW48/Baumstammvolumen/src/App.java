1a4d82c7da7605f49371efdf85555e62a98f835a
Silvio Brändle <silvio.braendle@noseryoung.com>
2023-11-29 09:34:27 +0100

    Add calculation for Baumstammvolumen

--- Java/2023/KW48/Baumstammvolumen/src/App.java
+++ Java/2023/KW48/Baumstammvolumen/src/App.java
@@ -4,7 +4,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Hello, World!");
+        System.out.println("--- Baumstammvolumen ---");
+
+        System.out.print("Durchmesser (cm): ");
+        double d = in.nextDouble();
+        in.nextLine();
+
+        System.out.print("Länge (m): ");
+        double l = in.nextDouble();
+        in.nextLine();
+
+        double v = (Math.PI * Math.pow(d, 2) * l) / 4 / 10000;
+
+        System.out.println("Volumen: " + Math.round(v * 1000) / 1000.0);
 
         in.close();
     }
