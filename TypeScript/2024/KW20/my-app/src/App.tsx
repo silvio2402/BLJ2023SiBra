b50824e2972e69c5a674aeb34537cd5f419a2f97
Silvio Braendle <silvio@silviobraendle.ch>
2024-05-15 15:32:14 +0200

    Add style and price calculation

--- TypeScript/2024/KW20/my-app/src/App.tsx
+++ TypeScript/2024/KW20/my-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import FruitPreview from "./FruitPreview";
 import { Fruit } from "./types";
@@ -31,12 +32,45 @@ const fruits: Fruit[] = [
 ];
 
 function App() {
+  const [counts, setCounts] = useState<Record<string, number>>(
+    fruits.reduce((acc, fruit) => ({ ...acc, [fruit.name]: 0 }), {})
+  );
+
   return (
-    <>
-      {fruits.map((fruit) => (
-        <FruitPreview {...fruit} />
-      ))}
-    </>
+    <div>
+      <div className="grid gap-4">
+        {fruits.map((fruit) => (
+          <FruitPreview
+            {...fruit}
+            onAdd={() =>
+              setCounts((prev) => ({
+                ...prev,
+                [fruit.name]: prev[fruit.name] + 1,
+              }))
+            }
+            onRemove={() =>
+              setCounts((prev) => ({
+                ...prev,
+                [fruit.name]: Math.max(0, prev[fruit.name] - 1),
+              }))
+            }
+            count={counts[fruit.name]}
+          />
+        ))}
+      </div>
+      <div className="pt-4">
+        <h1 className="text-2xl">Total</h1>
+        <p>
+          {Object.entries(counts)
+            .map(
+              ([name, count]) =>
+                fruits.find((fruit) => fruit.name === name)!.price * count
+            )
+            .reduce((acc, price) => acc + price, 0)}{" "}
+          CHF
+        </p>
+      </div>
+    </div>
   );
 }
 
--- TypeScript/2024/KW20/my-app/src/FruitPreview.tsx
+++ TypeScript/2024/KW20/my-app/src/FruitPreview.tsx
@@ -1,14 +1,73 @@
 import React from "react";
 import { Fruit } from "./types";
 
-type FruitProp = Fruit;
+type FruitProp = Fruit & {
+  count: number;
+  onAdd: () => void;
+  onRemove: () => void;
+};
 
-function FruitPreview({ name, image, price }: FruitProp) {
+function FruitPreview({
+  name,
+  image,
+  price,
+  count,
+  onAdd,
+  onRemove,
+}: FruitProp) {
   return (
-    <div className="text-blue-600">
-      <h1>{name}</h1>
-      <img src={image} />
-      <p>Price: {price}</p>
+    <div className="flex border border-gray-200 rounded-lg p-2 shadow-md">
+      <div className="flex gap-4 border-r-2 border-gray-200 pr-4">
+        <img src={image} className="h-16" />
+        <div>
+          <h1 className="text-lg">{name}</h1>
+          <p>{price} CHF</p>
+        </div>
+      </div>
+      <div className="flex-grow"></div>
+      <div className="flex flex-col gap-2 items-center border-l-2 border-gray-200 pl-2">
+        <p>{count}x</p>
+        <div className="flex gap-2">
+          <button
+            onClick={onAdd}
+            className="hover:bg-gray-200 active:bg-gray-300 rounded-md"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </button>
+          <button
+            onClick={onRemove}
+            className="hover:bg-gray-200 active:bg-gray-300 rounded-md"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M20 12H4"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
