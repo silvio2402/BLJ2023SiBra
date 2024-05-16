5297122fbfc55a5b11808cb4b98112649f615dc3
Silvio Braendle <silvio@silviobraendle.ch>
2024-05-16 10:21:51 +0200

    Use material UI

--- TypeScript/2024/KW20/my-app/src/FruitPreview.tsx
+++ TypeScript/2024/KW20/my-app/src/FruitPreview.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { Fruit } from "./types";
+import {
+  Box,
+  Card,
+  CardMedia,
+  Typography,
+  CardContent,
+  CardActions,
+  ButtonGroup,
+  Button,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 type FruitProp = Fruit & {
   count: number;
@@ -16,59 +28,36 @@ function FruitPreview({
   onRemove,
 }: FruitProp) {
   return (
-    <div className="flex border border-gray-200 rounded-lg p-2 shadow-md">
-      <div className="flex gap-4 border-r-2 border-gray-200 pr-4">
-        <img src={image} className="h-16" />
-        <div>
-          <h1 className="text-lg">{name}</h1>
-          <p>{price} CHF</p>
-        </div>
-      </div>
-      <div className="flex-grow"></div>
-      <div className="flex flex-col gap-2 items-center border-l-2 border-gray-200 pl-2">
-        <p>{count}x</p>
-        <div className="flex gap-2">
-          <button
-            onClick={onAdd}
-            className="hover:bg-gray-200 active:bg-gray-300 rounded-md"
+    <Card sx={{ display: "flex" }}>
+      <CardMedia component="img" image={image} sx={{ width: 151 }} />
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography component="div" variant="h5">
+            {name}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              />
-            </svg>
-          </button>
-          <button
-            onClick={onRemove}
-            className="hover:bg-gray-200 active:bg-gray-300 rounded-md"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M20 12H4"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </div>
+            {price} CHF
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h6" component="div">
+            {count}x
+          </Typography>
+          <ButtonGroup>
+            <Button onClick={onAdd} size="small">
+              <AddIcon />
+            </Button>
+            <Button onClick={onRemove} size="small">
+              <RemoveIcon />
+            </Button>
+          </ButtonGroup>
+        </CardActions>
+      </Box>
+    </Card>
   );
 }
 
