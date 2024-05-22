7793216a96e7c4c73248cfc6e23524a4d5ec7476
Silvio Braendle <silvio@silviobraendle.ch>
2024-05-22 09:55:49 +0200

    Complete uebung 2

--- TypeScript/2024/KW21/react-uebungen/src/App.tsx
+++ TypeScript/2024/KW21/react-uebungen/src/App.tsx
@@ -1,7 +1,24 @@
+import { AppBar, Box, Container, Grid } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Box p={2}>
+      <Container>
+        <AppBar />
+        <Grid container>
+          <Grid item xs={12}>
+            Form Here
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12}>
+            List Here
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
 
 export default App;
